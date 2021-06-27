1e359f97d2f1312cf92aac5a26acbfe870cb67b2
Paul Dechov <dechovsky@gmail.com>
2021-06-27 19:05:37 -0400

    Woo!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,20 @@
+// From https://github.com/Automattic/color-studio/blob/c84671390ebc7fb5ef616ba349b8ccadc533c745/dist/colors.json#L149-L161
+const woocommercePurple = {
+  DEFAULT: '#7f54b3',
+  0: '#f7edf7',
+  50: '#e5cfe8',
+  100: '#d6b4e0',
+  200: '#c792e0',
+  300: '#af7dd1',
+  400: '#9a69c7',
+  500: '#7f54b3',
+  600: '#674399',
+  700: '#533582',
+  800: '#3c2861',
+  900: '#271b3d',
+  1000: '#140e1f',
+};
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
@@ -6,4 +23,5 @@ module.exports = {
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
   ],
+  theme: { extend: { colors: { purple: woocommercePurple } } },
 };
